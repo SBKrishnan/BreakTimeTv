a33cedc7c91ca5eeca088b783c80aaae330c80c3
Bala Krishnan <balakrishnan19211@gmail.com>
2025-04-03 13:58:04 +0530

    Initialize React Native project structure and configuration files

---
+++ src/navigation/RootStack.tsx
@@ -0,0 +1,10 @@
+import {StyleSheet, Text, View} from 'react-native';
+const RootStack = () => {
+  return (
+    <View>
+      <Text>RootStack</Text>
+    </View>
+  );
+};
+export default RootStack;
+const styles = StyleSheet.create({});
